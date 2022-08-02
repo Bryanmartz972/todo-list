5d1a100f9fe6106507996eba3360c618f99dca56
Bryanmartz972 <bryanmartz972@gmail.com>
2022-08-01 21:55:12 -0600

    📦NEW: Button component completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,13 @@
-const Button = ({children, color}) => {
-  return (
-    <button className={`${color} flex items-center justify-center z-20 h-8 w-8 rounded-sm hover:${color} hover:brightness-90`}>{children}</button>
-  )
+const Button = ({ children, color, text, onClick }) => {
+	return (
+		<button onClick={onClick}
+			className={`${color} flex items-center justify-center z-20 p-1.5 rounded-sm hover:${color} hover:brightness-90`}>
+			<div className='flex items-center flex-row'>
+				{text && <span className='text-background font-bold mr-2'>{text}</span>}
+				{children}
+			</div>
+		</button>
+	)
 }
 
 export default Button
