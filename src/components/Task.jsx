bc1e956ae4fae8fc57df123de4625198346d398e
Bryanmartz972 <bryanmartz972@gmail.com>
2022-08-01 21:55:48 -0600

    📦NEW: Task component completed

---
+++ src/components/Task.jsx
@@ -0,0 +1,25 @@
+import Button from './Button'
+
+const Task = ({ id, icon, title, description }) => {
+	const deleteTask = () => {
+		const tasks = JSON.parse(localStorage.getItem('tasks'))
+		tasks.splice(id, 1)
+		localStorage.setItem('tasks', JSON.stringify(tasks))
+		window.location.reload(false)
+	}
+
+	return (
+		<button className='flex flex-row items-center rounded-md shadow-md p-4'>
+			<div className='rounded-full border w-3.5 h-3.5'></div>
+			<div className='grow w-[70%] text-start mx-3'>
+				<h2>{title}</h2>
+				<p>{description}</p>
+			</div>
+			<Button onClick={deleteTask} color='bg-secondary'>
+				{icon}
+			</Button>
+		</button>
+	)
+}
+
+export default Task
