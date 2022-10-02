80c01f9a6615ddac47d4d376b46abb2d3d82b39f
Bryanmartz972 <bryanmartz972@gmail.com>
2022-10-02 15:10:41 -0600

    UPDATE: Updated all project dependencies

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
 	watchForFileChanges: false,
 	e2e: {
-		baseUrl: 'http://localhost:5173',
+		baseUrl: 'http://localhost:5173/todo-list',
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
